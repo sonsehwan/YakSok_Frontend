bd4f87770872f144b4c63ae548b899c8d04f65b2
Son Sehwan <sahwan45@naver.com>
2026-09-01 20:00:21 +0900

    [refactor] 주석제거

--- app/src/main/java/com/example/medication/view/MapTouchContainer.java
+++ app/src/main/java/com/example/medication/view/MapTouchContainer.java
@@ -9,11 +9,6 @@ import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
-/**
- * 지도 SDK(MapView)는 내부 SurfaceView가 터치를 직접 소비해서, MapView에 OnTouchListener를 달아도
- * 호출되지 않는다. 그래서 MapView를 감싸는 이 컨테이너의 dispatchTouchEvent에서 자식에게 넘기기 전에
- * 조상 ScrollView의 가로채기를 미리 막아, 지도 위 드래그/핀치 줌이 스크롤과 겹치지 않게 한다.
- */
 public class MapTouchContainer extends FrameLayout {
 
     public MapTouchContainer(@NonNull Context context) {
